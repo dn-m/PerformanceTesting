88805cabbe3dbf13a2cdf767f8ca0f3facb075e7
James <jbean46@gmail.com>
2017-08-06 12:01:31 -0400

    Add stubs for stdlib tests

---
+++ Tests/PerformanceTestingTests/DictionaryTests.swift
@@ -0,0 +1,13 @@
+//
+//  DictionaryTests.swift
+//  PerformanceTestingTests
+//
+//  Created by James Bean on 8/6/17.
+//
+
+import XCTest
+import PerformanceTesting
+
+class DictionaryTests: PerformanceTestCase {
+
+}
---
+++ Tests/PerformanceTestingTests/SetTests.swift
@@ -0,0 +1,13 @@
+//
+//  SetTests.swift
+//  PerformanceTestingTests
+//
+//  Created by James Bean on 8/6/17.
+//
+
+import XCTest
+import PerformanceTesting
+
+class SetTests: PerformanceTestCase {
+
+}
