cfad8784101171ed092a330154061cff20613d78
Brian Heim <brianlheim@gmail.com>
2017-08-07 15:03:23 -0400

    add some set tests

--- Tests/PerformanceTestingTests/SetTests.swift
+++ Tests/PerformanceTestingTests/SetTests.swift
@@ -10,4 +10,26 @@ import PerformanceTesting
 
 class SetTests: PerformanceTestCase {
 
+    /// MARK - Helper functions.
+
+    // Constructs a set of size `n` with linearly increasing elements.
+    let constructSizeNSet: SetupFunction<Set<Int>> = { set, n in
+        set.reserveCapacity(Int(n))
+        for i in 0..<Int(n) {
+            set.insert(i)
+        }
+    }
+
+    /// MARK - Tests: inspecting
+
+    // `isEmpty` should be constant-time in the number of elements
+    func testIsEmpty() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, _ in _ = set.isEmpty }
+        )
+        assertConstantTimePerformance(data)
+    }
+
 }
