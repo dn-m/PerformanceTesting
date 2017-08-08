6c6fba3b0835fae98c8fb836139581a965c489b8
Brian Heim <brianlheim@gmail.com>
2017-08-07 20:34:26 -0400

    set tests: add tests

--- Tests/PerformanceTestingTests/SetTests.swift
+++ Tests/PerformanceTestingTests/SetTests.swift
@@ -32,4 +32,99 @@ class SetTests: PerformanceTestCase {
         assertConstantTimePerformance(data)
     }
 
+    // `count` should be constant-time in the number of elements
+    func testCount() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, _ in _ = set.count }
+        )
+        assertConstantTimePerformance(data)
+    }
+
+    // `first` should be constant-time in the number of elements
+    func testFirst() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, _ in _ = set.first }
+        )
+        assertConstantTimePerformance(data)
+    }
+
+    /// MARK - Tests: membership
+
+    // `contains` should be constant-time in the number of elements
+    func testContains() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, n in
+                let randomNumber = Int(arc4random_uniform(UInt32(n*2)))
+                for _ in 0..<100 {
+                    _ = set.contains(randomNumber)
+                }
+            }
+        )
+        assertConstantTimePerformance(data)
+    }
+
+    /// MARK - Tests: adding elements
+
+    // `insert` should be constant-time in the number of elements
+    func testInsert() {
+        let data = benchmarkMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, n in
+                for _ in 0..<10000 {
+                    let randomNumber = Int(arc4random_uniform(UInt32(n*2)))
+                    _ = set.insert(randomNumber)
+                }
+            }
+        )
+        assertConstantTimePerformance(data)
+    }
+
+    /// MARK - Tests: removing elements
+
+    // `filter` should be linear in the number of elements
+    func testFilter() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, n in
+                _ = set.filter { $0 % 5 == 3 }
+            }
+        )
+        assertPerformanceComplexity(data, complexity: .linear)
+    }
+
+    // `remove` should be constant-time in the number of elements
+    func testRemove() {
+        let data = benchmarkMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, n in
+                for _ in 0..<10000 {
+                    let randomNumber = Int(arc4random_uniform(UInt32(n*2)))
+                    _ = set.remove(randomNumber)
+                }
+            }
+        )
+        assertConstantTimePerformance(data)
+    }
+
+    // `removeFirst` should be constant-time in the number of elements
+    func testRemoveFirst() {
+        let data = benchmarkNonMutatingOperation(
+            mock: Set.init(),
+            setupFunction: constructSizeNSet,
+            trialCode: { set, n in
+                _ = set.removeFirst()
+            }
+        )
+        assertConstantTimePerformance(data)
+    }
+
 }
