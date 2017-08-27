2860ea80de78dcebb22d17ea4cc3f27f3d6bc2d5
Brian Heim <brianlheim@gmail.com>
2017-08-27 16:14:34 -0400

    add generator init for Set, use in SetTests

--- Sources/PerformanceTesting/PerformanceTestCase.swift
+++ Sources/PerformanceTesting/PerformanceTestCase.swift
@@ -172,3 +172,15 @@ extension Array {
     }
 
 }
+
+extension Set {
+
+    /// Fills the array using a generator function. The function
+    /// is passed the index of the current element as an argument.
+    public init(count: Int, fillingWith elementGenerator: (Int) -> Element) {
+        self.init()
+        reserveCapacity(count)
+        for i in 0..<count { insert(elementGenerator(i)) }
+    }
+
+}
