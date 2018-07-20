b9b66118894c54392b438a3c93eb46a97e3846cd
James <jbean46@gmail.com>
2018-07-20 14:53:39 -0700

    Add set: (FillStrategy) -> (Int) -> Set<Int>

--- Tests/PerformanceTestingTests/SetTests.swift
+++ Tests/PerformanceTestingTests/SetTests.swift
@@ -14,19 +14,19 @@ class SetTests: XCTestCase {
 
     // `isEmpty` should be constant-time in the number of elements
     func testIsEmpty() {
-        let benchmark = Benchmark.nonMutating(setup: makeSet) { _ = $0.isEmpty }
+        let benchmark = Benchmark.nonMutating(setup: set(.increasing)) { _ = $0.isEmpty }
         assertPerformance(.constant, of: benchmark)
     }
 
     // `count` should be constant-time in the number of elements
     func testCount() {
-        let benchmark = Benchmark.nonMutating(setup: makeSet) { _ = $0.count }
+        let benchmark = Benchmark.nonMutating(setup: set(.increasing)) { _ = $0.count }
         assertPerformance(.constant, of: benchmark)
     }
 
     // `first` should be constant-time in the number of elements
     func testFirst() {
-        let benchmark = Benchmark.nonMutating(setup: makeSet) { _ = $0.first }
+        let benchmark = Benchmark.nonMutating(setup: set(.increasing)) { _ = $0.first }
         assertPerformance(.constant, of: benchmark)
     }
 
@@ -35,7 +35,7 @@ class SetTests: XCTestCase {
     // `contains` should be constant-time in the number of elements
     // if the element in question is in the set
     func testContainsHit() {
-        let benchmark = Benchmark.nonMutating(setup: makeSet) {
+        let benchmark = Benchmark.nonMutating(setup: set(.increasing)) {
             _ = $0.contains(Int.random(in: 0..<$0.count))
         }
         assertPerformance(.constant, of: benchmark)
@@ -44,7 +44,7 @@ class SetTests: XCTestCase {
     // `contains` should be constant-time in the number of elements
     // if the element in question is in the set
     func testContainsMiss() {
-        let benchmark = Benchmark.nonMutating(setup: makeSet) { _ = $0.contains($0.count) }
+        let benchmark = Benchmark.nonMutating(setup: set(.increasing)) { _ = $0.contains($0.count) }
         assertPerformance(.constant, of: benchmark)
     }
 
@@ -52,7 +52,7 @@ class SetTests: XCTestCase {
 
     // `insert` should be constant-time in the number of elements
     func testInsert() {
-        let benchmark = Benchmark.mutating(setup: makeSet) {
+        let benchmark = Benchmark.mutating(setup: set(.increasing)) {
             $0.insert(Int.random(in: 0 ..< $0.count * 2))
         }
         assertPerformance(.constant, of: benchmark)
@@ -62,27 +62,27 @@ class SetTests: XCTestCase {
 
     // `filter` should be linear in the number of elements
     func testFilter() {
-        let benchmark = Benchmark.mutating(setup: makeSet) { _ = $0.filter { $0 % 5 == 3 } }
+        let benchmark = Benchmark.mutating(setup: set(.increasing)) { _ = $0.filter { $0 % 5 == 3 } }
         assertPerformance(.linear, of: benchmark)
     }
 
     // `remove` should be constant-time in the number of elements,
     // if the element to be removed is in the set
     func testRemoveHit() {
-        let benchmark = Benchmark.mutating(setup: makeSet) { $0.remove(Int.random(in: 0..<$0.count)) }
+        let benchmark = Benchmark.mutating(setup: set(.increasing)) { $0.remove(Int.random(in: 0..<$0.count)) }
         assertPerformance(.constant, of: benchmark)
     }
 
     // `remove` should be constant-time in the number of elements,
     // if the element to be removed is not in the set
     func testRemoveMiss() {
-        let benchmark = Benchmark.mutating(setup: makeSet) { $0.remove($0.count) }
+        let benchmark = Benchmark.mutating(setup: set(.increasing)) { $0.remove($0.count) }
         assertPerformance(.constant, of: benchmark)
     }
 
     // `removeFirst` should be constant-time in the number of elements
     func testRemoveFirst() {
-        let benchmark = Benchmark.mutating(setup: makeSet) { $0.removeFirst() }
+        let benchmark = Benchmark.mutating(setup: set(.increasing)) { $0.removeFirst() }
         assertPerformance(.constant, of: benchmark)
     }
 
@@ -107,7 +107,21 @@ class SetTests: XCTestCase {
     }
 }
 
+func set (_ strategy: FillStrategy) -> (_ size: Int) -> Set<Int> {
+    switch strategy {
+    case .increasing:
+        return makeSet
+    case .random:
+        return makeRandomSet
+    }
+}
+
 // Constructs a set of size `n` with linearly increasing elements.
 func makeSet(size n: Int) -> Set<Int> {
     return Set(count: n) { $0 }
+}
+
+// Constructs an array of size `n` with random elements.
+func makeRandomSet(size n: Int) -> Set<Int> {
+    return Set(count: n) { _ in n.random() }
 }
