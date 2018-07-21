07dd28d12b1bdde7acd79ab54d68418b09d5b4da
James <jbean46@gmail.com>
2018-07-21 10:09:37 -0700

    Remove assertPerformanceComplexity and constant friend

--- Sources/PerformanceTesting/PerformanceTestCase.swift
+++ Sources/PerformanceTesting/PerformanceTestCase.swift
@@ -8,40 +8,6 @@
 import XCTest
 import Darwin
 
-/// Assert that the data indicates that performance is constant-time ( O(1) ).
-internal func assertConstantTimePerformance(
-    _ benchmark: [(Double,Double)],
-    tolerance: Double = 0.01
-)
-{
-    let results = linearRegression(benchmark)
-    guard approximatelyEqual(results.slope, 0, epsilon: tolerance) || results.correlation < 0.9 else {
-        dump(benchmark)
-        dump(results)
-        XCTFail("Operation not observed in constant time")
-        return
-    }
-}
-
-/// Assert that the data indicates that performance fits well to the given
-/// complexity class. Optional parameter for minimum acceptable correlation.
-/// Use assertConstantTimePerformance for O(1) assertions
-internal func assertPerformanceComplexity(
-    _ benchmark: [(Double,Double)],
-    complexity: Complexity,
-    minimumCorrelation: Double = 0.9
-)
-{
-    let mappedData = benchmark.mappedForLinearFit(complexity: complexity)
-    let results = linearRegression(mappedData)
-    guard results.correlation >= minimumCorrelation else {
-        dump(benchmark)
-        dump(results)
-        XCTFail("Operation not observed in \(complexity) time")
-        return
-    }
-}
-
 extension Array where Element == (Double, Double) {
 
     /// Maps data representing performance of a certain complexity so that it
