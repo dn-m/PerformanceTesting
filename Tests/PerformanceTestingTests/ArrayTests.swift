653705032d12b3872a07fde8c444306e31ecca3c
James <jbean46@gmail.com>
2018-07-21 08:39:16 -0700

    Move array: (FillStrategy) -> (Int) -> [Int] to global testing location

--- Tests/PerformanceTestingTests/ArrayTests.swift
+++ Tests/PerformanceTestingTests/ArrayTests.swift
@@ -90,12 +90,3 @@ class ArrayTests: XCTestCase {
         assertPerformance(.linear, of: benchmark)
     }
 }
-
-private func array (_ strategy: FillStrategy) -> (_ size: Int) -> Array<Int> {
-    switch strategy {
-    case .increasing:
-        return { size in .init(0..<size) }
-    case .random:
-        return { size in .init((0..<size).map { _ in Int.random(in: 0..<size) }) }
-    }
-}
--- Tests/PerformanceTestingTests/StructurePreparation.swift
+++ Tests/PerformanceTestingTests/StructurePreparation.swift
@@ -9,3 +9,12 @@ enum FillStrategy {
     case increasing
     case random
 }
+
+private func array (_ strategy: FillStrategy) -> (_ size: Int) -> Array<Int> {
+    switch strategy {
+    case .increasing:
+        return { size in .init(0..<size) }
+    case .random:
+        return { size in .init((0..<size).map { _ in Int.random(in: 0..<size) }) }
+    }
+}
